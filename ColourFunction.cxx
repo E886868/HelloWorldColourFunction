8c2cf0182cda6948a32df8c507b010fb3b09f326
Caroline Thomas <caroline_thomas_@hotmaill.co.uk>
2020-06-09 16:22:02 +0100

    Added enum, cleaned main function

--- ColourFunction.cxx
+++ ColourFunction.cxx
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-char colour(x)
+enum colours {BLUE, RED, CYAN, YELLOW, MAGENTA, GREEN, RESET};
+void set_text_colour(enum colours colour)
 {
-	if (x=="b")
-	{
-		printf("\033[0;34m");
-	}
+       switch (colour)
+       {
+       case BLUE:
+		 printf("\033[0;34m");
+             break;
+       case RED:
+		printf("\033[0;31m");
+             break;
+       case CYAN:
+		printf("\033[0;36m");
+             break;
+       case YELLOW:
+		printf("\033[0;33m");
+             break;
+       case MAGENTA:
+		printf("\033[35m");
+             break;
+       case GREEN:
+		printf("\033[32m");
+             break;
+       case  RESET:
+		printf("\033[0m");
+             break;
+       }
 }
+
 
 
 int main()
 {
+	set_text_colour(BLUE);
+	printf("I am blue\n");
 
-	strcpy(str1, colour(b));
-
-	//char str1[1];
-
-	//strcpy(str1, "\033[0;34m");
-
-	strcat(str1, "Hello World\n");
-
-	printf(str1);
+ 	set_text_colour(RED);
+	printf("I am RED\n");
 
 	return 0;
 }
