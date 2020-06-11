11b9df8d74f3653b179caddf571e1311c97e791b
Caroline Thomas <caroline_thomas_@hotmaill.co.uk>
2020-06-11 11:38:50 +0100

    Code tidy, print all HEX

--- OpenFile.cxx
+++ OpenFile.cxx
@@ -2,52 +2,18 @@
 int main()
 {
 FILE *fp;
-int ch, i;
+int ch, i, b;
 fp=fopen("test.DMD","r");
 
-for (i = 1; i < 17; i++)
-  {
-	ch = fgetc (fp);
-	printf("%02x ",ch);
-
-  }
-
-printf("\n");
-
-for (i = 1; i < 17; i++)
+for (b = 1; b < 16; b++)
+{
+	for (i = 1; i < 17; i++)
   {
         ch = fgetc (fp);
         printf("%02x ",ch);
-
   }
-
-printf("\n");
-
-for (i = 1; i < 17; i++)
-  {
-        ch = fgetc (fp);
-        printf("%02x ",ch);
-
-  }
-
-printf("\n");
-
-for (i = 1; i < 17; i++)
-  {
-        ch = fgetc (fp);
-        printf("%02x ",ch);
-
-  }
-
-printf("\n");
-
-for (i = 1; i < 17; i++)
-  {
-        ch = fgetc (fp);
-        printf("%02x ",ch);
-
-  }
-
+	printf("\n");
+}
 
 fclose(fp);
 return 0;
